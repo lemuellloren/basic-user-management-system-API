45ac6b4ccf8febe70cb978fb3e19b1aaa4ce00e9
lemuellloren <lempakz@yahoo.com>
2022-01-24 10:18:44 +0800

    set up users controller

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,71 @@
+'use strict';
+const Users = require('../models/users.model');
+
+
+exports.findAll = function (req, res) {
+    Users.findAll(function (err, users) {
+        console.log('controller')
+        if (err)
+            res.send(err);
+        console.log('res', users);
+        res.send(users);
+    });
+};
+
+exports.create = function (req, res) {
+    const new_users = new Users(req.body);
+    //* handles null error
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        res.status(400).send({
+            error: true,
+            message: 'Please provide all required field'
+        });
+    } else {
+        Users.create(new_users, function (err, users) {
+            if (err)
+                res.send(err);
+            res.json({
+                error: false,
+                message: "User added successfully!",
+                data: users
+            });
+        });
+    }
+};
+
+exports.findById = function (req, res) {
+    Users.findById(req.params.id, function (err, users) {
+        if (err)
+            res.send(err);
+        res.json(users);
+    });
+};
+
+exports.update = function (req, res) {
+    if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+        res.status(400).send({
+            error: true,
+            message: 'Please provide all required field'
+        });
+    } else {
+        Users.update(req.params.id, new users(req.body), function (err, users) {
+            if (err)
+                res.send(err);
+            res.json({
+                error: false,
+                message: 'User successfully updated'
+            });
+        });
+    }
+};
+
+exports.delete = function (req, res) {
+    Users.delete(req.params.id, function (err, users) {
+        if (err)
+            res.send(err);
+        res.json({
+            error: false,
+            message: 'User successfully deleted'
+        });
+    });
+};
